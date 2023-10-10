9c8d8588873ee0754019926f242fb8e34b72698d
kimpamela-1239506 <139266345+kimpamela-1239506@users.noreply.github.com>
2023-10-10 17:36:03 -0600

    Creación de graficas 1

---
+++ ejemploproyecto4tobaco_kpdm/src/ejemploproyecto4tobaco_kpdm/Ejemploproyecto4tobaco_kpdm.java
@@ -0,0 +1,12 @@
+package ejemploproyecto4tobaco_kpdm;
+
+public class Ejemploproyecto4tobaco_kpdm {
+
+    public static void main(String[] args) {
+        ventana inicio = new ventana();
+        inicio.setBounds(350, 150, 550, 400);
+        inicio.setTitle("Inicio sesion");
+        inicio.iniciarComponentes();
+    }
+
+}
